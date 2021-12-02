c4fcd2bfd698ab05811159c52c7ad470c49d5cfb
Raghav <raghav.kanu26@gmail.com>
2021-12-02 18:26:44 +0530

    PythonCodes:3sums

--- 3 sums problem.py
+++ 3 sums problem.py
@@ -1,5 +1,25 @@
-'''
+
+#The sum of 3 value must be equal to zero
+
+arr=[-1,0,1,2,-1,-4]
+arr.sort()
+result=[]
+for i,a in enumerate(arr):
+    if i>0 and arr[i-1]==arr[i]:
+        continue
+    low=i
+    high=len(arr)-1
+    while(low<high):
+        total=arr[low]+arr[high]+a
+        if total<0:
+            low+=1
+        elif total>0:
+            high-=1
+        else:
+            result.append([arr[low],arr[high],a])
+            low+=1
+            while(arr[low]==arr[low-1] and low<high):
+                low+=1
 
 
-Hi this is a test file
-'''
+print(result)
