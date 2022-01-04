4f73cc5bfa256bcff3fd00cb3150a611c934ced3
Raghav <raghav.kanu26@gmail.com>
2022-01-04 18:27:50 +0530

    Database Management System

--- Database Management System.py
+++ Database Management System.py
@@ -7,15 +7,21 @@ class student:
         self.age=int(input("Kindly enter the age: "))
 
         try:
-            cursor = conn.execute(f'select max(Roll) from students where Class={self.classroom}')
+            cursor = conn.execute('select max(Roll) from students where Class=?',(self.classroom,))
         except Exception as e:
+            print("exception")
             self.roll=1
         else:
-            print(cursor.fetchone())
-            if cursor.fetchone() is not None:
-                self.roll = int(list(cursor.fetchone())[0])+1
-            else:
+            val=cursor.fetchone()
+            print(val)
+            if val is None:
                 self.roll = 1
+                print("here")
+            else:
+                self.roll = val[0] + 1
+                print(val[0],self.roll)
+                print("not")
+
         path='E:\\Python\\Pycharm Programs'
         self.remarks_path = str(self.name)+str(self.classroom)+str(self.roll)
         self.marksheet_path = str(self.name)+str(self.classroom)+str(self.roll)+'Marksheet'
@@ -23,15 +29,20 @@ class student:
         self.insertdb()
 
     def insertdb(self):
-        #try:
+        try:
             conn.execute(f'INSERT INTO students VALUES(?,?,?,?,?,?)',(self.roll,self.name,self.age,self.remarks_path,self.classroom,self.marksheet_path))
-        #except Exception as e:
-        #    print("Unable to write in database",e)
+        except Exception as e:
+            print("Unable to write in database",e)
+        else:
+            conn.commit()
 
-       # else:
-        #    conn.commit()
-
-
+    def display(self,roll):
+        try:
+            cursor=conn.execute('select * from students where roll=?',(roll,))
+        except Exception as e:
+            print("Unable to fetch",e)
+        else:
+            print(cursor.fetchone())
 
 if __name__=="__main__":
 
@@ -57,3 +68,6 @@ if __name__=="__main__":
     cursor=conn.execute('select * from students')
     print(cursor.fetchall())
     stud1=student()
+    stud1.display(1)
+    cursor = conn.execute('select * from students')
+    print(cursor.fetchall())
